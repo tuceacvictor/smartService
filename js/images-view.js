e2d04b18e456c800c55d43621096cbe080333065
tuceacvictor <tuceak16@gmail.com>
2019-12-04 22:50:10 +0200

    prices modals

--- js/images-view.js
+++ js/images-view.js
@@ -4,3 +4,28 @@ $(function() {
         $('#imagemodal').modal('show');
     });
 });
+
+
+$(function() {
+    $('.apple-price-btn').on('click', function() {
+       
+        $('#apple-prices-modal').modal('show');
+    });
+});
+
+
+
+$(function() {
+    $('.samsung-price-btn').on('click', function() {
+      
+        $('#samsung-prices-modal').modal('show');
+    });
+});
+
+
+$(function() {
+    $('.other-price-btn').on('click', function() {
+       
+        $('#other-prices-modal').modal('show');
+    });
+});
