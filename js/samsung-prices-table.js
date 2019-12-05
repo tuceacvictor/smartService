c27fb5d741e19e0a1ad1940d8fccb415de5122fd
tuceacvictor <tuceak16@gmail.com>
2019-12-06 00:48:32 +0200

    excel parsing

--- js/samsung-prices-table.js
+++ js/samsung-prices-table.js
@@ -9,110 +9,134 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var Prices = function (_React$Component) {
-  _inherits(Prices, _React$Component);
+    _inherits(Prices, _React$Component);
 
-  function Prices(props) {
-    _classCallCheck(this, Prices);
+    function Prices(props) {
+        _classCallCheck(this, Prices);
 
-    var _this = _possibleConstructorReturn(this, (Prices.__proto__ || Object.getPrototypeOf(Prices)).call(this, props));
+        var _this = _possibleConstructorReturn(this, (Prices.__proto__ || Object.getPrototypeOf(Prices)).call(this, props));
 
-    _this.state = {};
-    return _this;
-  }
+        _this.readFile = function (file) {
+            fetch(file, {
+                headers: {
+                    contentType: "application/vnd.ms-excel; charset=utf-8"
+                }
+            }).then(function (response) {
+                return response.arrayBuffer();
+            }).then(function (resp) {
+                return console.log(resp);
+            });
+            // .then(response => response.arrayBuffer())
+            // .then(ab => {
+            //   // do stuff with `ArrayBuffer` representation of file
+            // })
+            // .catch(err => console.log(err));
+        };
 
-  _createClass(Prices, [{
-    key: "componentDidMount",
-    value: function componentDidMount() {}
-  }, {
-    key: "render",
-    value: function render() {
-
-      return React.createElement(
-        "table",
-        { className: "table" },
-        React.createElement(
-          "thead",
-          null,
-          React.createElement(
-            "tr",
-            null,
-            React.createElement(
-              "th",
-              { scope: "col" },
-              "\u041D\u0430\u0438\u043C\u0435\u043D\u043E\u0432\u0430\u043D\u0438\u0435"
-            ),
-            React.createElement(
-              "th",
-              { scope: "col" },
-              "\u0426\u0435\u043D\u0430"
-            )
-          )
-        ),
-        React.createElement(
-          "tbody",
-          null,
-          React.createElement(
-            "tr",
-            null,
-            React.createElement(
-              "th",
-              { scope: "row" },
-              "Iphone 11"
-            ),
-            React.createElement(
-              "th",
-              { scope: "row" },
-              "59000"
-            )
-          ),
-          React.createElement(
-            "tr",
-            null,
-            React.createElement(
-              "th",
-              { scope: "row" },
-              "Iphone 11"
-            ),
-            React.createElement(
-              "th",
-              { scope: "row" },
-              "59000"
-            )
-          ),
-          React.createElement(
-            "tr",
-            null,
-            React.createElement(
-              "th",
-              { scope: "row" },
-              "Iphone 11"
-            ),
-            React.createElement(
-              "th",
-              { scope: "row" },
-              "59000"
-            )
-          ),
-          React.createElement(
-            "tr",
-            null,
-            React.createElement(
-              "th",
-              { scope: "row" },
-              "Iphone 11"
-            ),
-            React.createElement(
-              "th",
-              { scope: "row" },
-              "59000"
-            )
-          )
-        )
-      );
+        _this.state = {};
+        return _this;
     }
-  }]);
 
-  return Prices;
+    _createClass(Prices, [{
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            var data = '';
+            this.readFile('./js/receipts.xls');
+            var workbook = XLSX.read(data, {
+                type: "binary"
+            });
+            console.log(workbook);
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+
+            return React.createElement(
+                'table',
+                { className: 'table' },
+                React.createElement(
+                    'thead',
+                    null,
+                    React.createElement(
+                        'tr',
+                        null,
+                        React.createElement(
+                            'th',
+                            { scope: 'col' },
+                            '\u041D\u0430\u0438\u043C\u0435\u043D\u043E\u0432\u0430\u043D\u0438\u0435'
+                        ),
+                        React.createElement(
+                            'th',
+                            { scope: 'col' },
+                            '\u0426\u0435\u043D\u0430'
+                        )
+                    )
+                ),
+                React.createElement(
+                    'tbody',
+                    null,
+                    React.createElement(
+                        'tr',
+                        null,
+                        React.createElement(
+                            'th',
+                            { scope: 'row' },
+                            'Iphone 11'
+                        ),
+                        React.createElement(
+                            'th',
+                            { scope: 'row' },
+                            '59000'
+                        )
+                    ),
+                    React.createElement(
+                        'tr',
+                        null,
+                        React.createElement(
+                            'th',
+                            { scope: 'row' },
+                            'Iphone 11'
+                        ),
+                        React.createElement(
+                            'th',
+                            { scope: 'row' },
+                            '59000'
+                        )
+                    ),
+                    React.createElement(
+                        'tr',
+                        null,
+                        React.createElement(
+                            'th',
+                            { scope: 'row' },
+                            'Iphone 11'
+                        ),
+                        React.createElement(
+                            'th',
+                            { scope: 'row' },
+                            '59000'
+                        )
+                    ),
+                    React.createElement(
+                        'tr',
+                        null,
+                        React.createElement(
+                            'th',
+                            { scope: 'row' },
+                            'Iphone 11'
+                        ),
+                        React.createElement(
+                            'th',
+                            { scope: 'row' },
+                            '59000'
+                        )
+                    )
+                )
+            );
+        }
+    }]);
+
+    return Prices;
 }(React.Component);
 
 var domContainer = document.querySelector('#samsung-prices-table');
--- src/samsung-prices-table.js
+++ src/samsung-prices-table.js
@@ -1,54 +1,68 @@
 'use strict';
+
 class Prices extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    readFile = (file) => {
+        fetch(file, {
+            headers: {
+              contentType: "application/vnd.ms-excel; charset=utf-8",
+            },
+        }).then(response => response.arrayBuffer()).then(resp => console.log(resp))
+        // .then(response => response.arrayBuffer())
+        // .then(ab => {
+        //   // do stuff with `ArrayBuffer` representation of file
+        // })
+        // .catch(err => console.log(err));
+    }
+
+    componentDidMount() {
+        let data = '';
+        this.readFile('./js/receipts.xls');
+        let workbook = XLSX.read(data, {
+            type: "binary"
+        });
+        console.log(workbook)
+    }
 
 
-    };
-  }
+    render() {
 
+        return (
+            <table className="table">
+                <thead>
+                <tr>
+                    <th scope="col">Наименование</th>
+                    <th scope="col">Цена</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">Iphone 11</th>
+                    <th scope="row">59000</th>
+                </tr>
+                <tr>
+                    <th scope="row">Iphone 11</th>
+                    <th scope="row">59000</th>
+                </tr>
+                <tr>
+                    <th scope="row">Iphone 11</th>
+                    <th scope="row">59000</th>
+                </tr>
+                <tr>
+                    <th scope="row">Iphone 11</th>
+                    <th scope="row">59000</th>
+                </tr>
+                </tbody>
+            </table>
 
+        )
 
-  componentDidMount() {
-   
-  }
-
-
-  render() {
-
-    return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Наименование</th>
-            <th scope="col">Цена</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">Iphone 11</th>
-            <th scope="row">59000</th>
-          </tr>
-          <tr>
-            <th scope="row">Iphone 11</th>
-            <th scope="row">59000</th>
-          </tr>
-          <tr>
-            <th scope="row">Iphone 11</th>
-            <th scope="row">59000</th>
-          </tr>
-          <tr>
-            <th scope="row">Iphone 11</th>
-            <th scope="row">59000</th>
-          </tr>
-        </tbody>
-      </table>
-
-    )
-
-  }
+    }
 }
 
 const domContainer = document.querySelector('#samsung-prices-table');
-ReactDOM.render(<Prices />, domContainer);
+ReactDOM.render(<Prices/>, domContainer);
